b8d9e01767f4d24b36b9fef6e2545495cbab7dcc
Aksi0322 <126950051+Aksi0322@users.noreply.github.com>
2024-05-18 20:15:55 +0530

    create and run the server

---
+++ client/src/pages/Admin.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Admin() {
+  return (
+    <article className="text-wrap font-bold text-center">
+      <div class='bg-cover shadow-sm h-screen bg-[url("https://ctexaminer.com/wp-content/uploads/2020/09/AdobeStock_214316329-scaled-1.jpeg")]'>
+        <div className="text-center">
+          <span className="text-black text-lg">Welcome To Admin Login!</span>
+        </div>
+      </div>
+    </article>
+  );
+}
