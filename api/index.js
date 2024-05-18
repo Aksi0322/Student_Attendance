59e0d5ad748b9c58aa093606d56bc16701d1307d
Aksi0322 <126950051+Aksi0322@users.noreply.github.com>
2024-05-18 20:41:04 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,13 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb+srv://Akshay:Akshay@studentattendance.ygl6ygz.mongodb.net/?retryWrites=true&w=majority&appName=StudentAttendance"
+).then;
+console.log("Connected to MongoDB");
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log("server is running on port 3000!!!!");
+  console.log("server is running on port 3000!");
 });
